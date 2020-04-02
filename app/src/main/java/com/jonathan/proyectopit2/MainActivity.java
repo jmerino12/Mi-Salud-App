553f9f8f6814dd31b2f47c239f3f252735025529
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2020-04-02 16:53:36 -0500

    SplashScreen finish

---
+++ app/src/main/java/com/jonathan/proyectopit2/MainActivity.java
@@ -0,0 +1,57 @@
+package com.jonathan.proyectopit2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 5000;
+    //Varibales animation
+    Animation topAnim, bottomAnim;
+    ImageView imagen;
+    TextView logo,slogan;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+     //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        imagen = findViewById(R.id.imageView);
+        logo = findViewById(R.id.nombreapp);
+        slogan = findViewById(R.id.slogan);
+
+        imagen.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+    }
+
+
+}
