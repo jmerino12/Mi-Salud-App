c894f38576fca07bdeea3d0adb1df9ad6b6dd4be
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2020-04-03 10:57:05 -0500

    Activity Registro

--- app/src/main/java/com/jonathan/proyectopit2/Login.java
+++ app/src/main/java/com/jonathan/proyectopit2/Login.java
@@ -2,15 +2,28 @@ package com.jonathan.proyectopit2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    Button registrarme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        registrarme = findViewById(R.id.btn_registrarse);
+
+        registrarme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,SingUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
